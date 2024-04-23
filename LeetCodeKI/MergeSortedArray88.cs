6c5afe4643de3a18b986723a847afb44d82b41da
GeorgiyGKI <georgiygki@gmail.com>
2024-04-23 20:51:51 +0300

    Add project files.

---
+++ LeetCodeKI/MergeSortedArray88.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeKI
+{
+    public class MergeSortedArray88
+    {
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            //not my solution
+            if (n == 0) return;
+            int len1 = nums1.Length;
+            int end_idx = len1 - 1;
+            while (n > 0 && m > 0)
+            {
+                if (nums2[n - 1] >= nums1[m - 1])
+                {
+                    nums1[end_idx] = nums2[n - 1];
+                    n--;
+                }
+                else
+                {
+                    nums1[end_idx] = nums1[m - 1];
+                    m--;
+                }
+                end_idx--;
+            }
+            while (n > 0)
+            {
+                nums1[end_idx] = nums2[n - 1];
+                n--;
+                end_idx--;
+            }
+
+            //MY solution)0)
+            var newSize = m + n;
+            Array.Resize(ref nums1, newSize);
+            Array.Copy(nums2, 0, nums1, m, n);
+            Array.Sort(nums1);
+        }
+    }
+}
+
---
+++ LeetCodeKI/Program.cs
@@ -0,0 +1,9 @@
+﻿using LeetCodeKI;
+
+//MergeSortedArray  88
+MergeSortedArray88 mergeSortedArray88 = new MergeSortedArray88();
+var nums1 = new int[] { 1, 2, 3, 0, 0, 0, 0};
+var nums2 = new int[] { 2, 5, 6, 4 };
+var m = 3;
+var n = 3;
+mergeSortedArray88.Merge(nums1, m, nums2, n);
