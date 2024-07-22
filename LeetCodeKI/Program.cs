e578108e7e471aa54cb49aa7f03a8abeb8b6caea
GeorgiyGKI <georgiygki@gmail.com>
2024-07-22 06:50:26 +0300

    MoveZeroesMethod

--- LeetCodeKI/Program.cs
+++ LeetCodeKI/Program.cs
@@ -1,25 +1,30 @@
 ﻿using LeetCodeKI.interview;
+using LeetCodeKI.other;
 
-//MergeSortedArray  88
-var nums1 = new int[] { 1, 2, 3, 0, 0, 0, 0};
-var nums2 = new int[] { 2, 5, 6, 4 };
-var m = 3;
-var n = 3;
-MergeSortedArray88.Merge(nums1, m, nums2, n);
+////MergeSortedArray  88
+//var nums1 = new int[] { 1, 2, 3, 0, 0, 0, 0};
+//var nums2 = new int[] { 2, 5, 6, 4 };
+//var m = 3;
+//var n = 3;
+//MergeSortedArray88.Merge(nums1, m, nums2, n);
 
-//TwoSum 2
-var nums = new int[] { -3, 4, 3, 90 };
-var target = 0;
-TwoSum2.TwoSum(nums, target);
+////TwoSum 2
+//var nums = new int[] { -3, 4, 3, 90 };
+//var target = 0;
+//TwoSum2.TwoSum(nums, target);
 
-//RemoveElement 27
-var nums27 = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
-var val27 = 2;
-RemoveElement27.RemoveElement(nums27, val27);
+////RemoveElement 27
+//var nums27 = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 };
+//var val27 = 2;
+//RemoveElement27.RemoveElement(nums27, val27);
 
-//BestTimetoBuyandSellStock 121
-var nums121 = new int[] { 7, 2, 3 , 6, 8, 1, 6 };
-BestTimetoBuyandSellStock.MaxProfit(nums121);
+////BestTimetoBuyandSellStock 121
+//var nums121 = new int[] { 7, 2, 3 , 6, 8, 1, 6 };
+//BestTimetoBuyandSellStock.MaxProfit(nums121);
 
-//13. Roman to Integer
-RomanToInteger.RomanToInt("MCMXCIV");
+////13. Roman to Integer
+//RomanToInteger.RomanToInt("MCMXCIV");
+
+var zeroze = new int[] { 1, 0, 12, 0, 0 };
+MoveZeroes.MoveZeroesMethod(zeroze);
+Console.WriteLine(zeroze);
---
+++ LeetCodeKI/other/MoveZeroes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeKI.other
+{
+	public static class MoveZeroes
+	{
+		public static void MoveZeroesMethod(int[] nums)
+		{
+			var pointer = 0;
+			for (int i = 0; i < nums.Length; i++) {
+				if (nums[i] != 0)
+				{
+					(nums[i], nums[pointer]) = (nums[pointer], nums[i]);
+					pointer++;
+				}
+			}
+		}
+	}
+}
